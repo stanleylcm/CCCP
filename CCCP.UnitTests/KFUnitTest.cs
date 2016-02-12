1d1acd8620d6ea862c55fdd2a77a530faf114f5f
Stanley Lam <stanley_lam@kinetix.com.hk>
2016-02-12 11:04:37 +0800

    draft

---
+++ CCCP.UnitTests/KFUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CCCP.Models;
+using System.Data.Entity.Core.Objects;
+
+namespace CCCP.UnitTests
+{
+    [TestClass]
+    public class KFUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
