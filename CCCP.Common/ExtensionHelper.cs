b5ca31c85ecf43d0f18b1ecedfcfea1ea8fcdfde
Stanley Lam <stanley_lam@kinetix.com.hk>
2016-02-12 11:47:08 +0800

    draft

---
+++ CCCP.Common/ExtensionHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCCP.Common
+{
+    public static class ExtensionHelper
+    {
+        public static bool IsEquals(this string self, string compareStr)
+        {
+            return (self.Trim().ToUpper() == compareStr.Trim().ToUpper());
+        }
+    }
+
+}
