5cf1abb78e62fe48a0a5444db27705db3ad87c4a
kf-wang <kfwang4@gmail.com>
2016-02-26 14:41:30 +0800

    draft ( System (Invoicing) )

---
+++ CCCP.Business/Model/IncidentSystemInvoicingModel.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CCCP.Common;
+using CCCP.ViewModel;
+using CCCP.Business.Service;
+
+namespace CCCP.Business.Model
+{
+    public class IncidentSystemInvoicingModel
+    {
+        #region Constructor
+
+        public IncidentSystemInvoicingModel()
+        {
+            init();
+        }
+        public IncidentSystemInvoicingModel(IncidentSystemInvoicing viewModel)
+        {
+            init();
+            this.Entity = viewModel;
+        }
+
+        private void init()
+        {
+            initOptions();
+        }
+
+        #endregion
+
+        #region Declaration
+
+        public IncidentSystemInvoicing Entity = new IncidentSystemInvoicing();
+        public List<ChecklistModel> Checklists = new List<ChecklistModel>();
+        public List<Checklist> ChecklistEntities
+        {
+            set
+            {
+                Checklists = new List<ChecklistModel>();
+                foreach (Checklist checklistEntity in value) Checklists.Add(new ChecklistModel(checklistEntity));
+            }
+        }
+
+        public List<string> Options_ProblemArea = new List<string>();
+        public List<string> Options_PossibleCause = new List<string>();
+        public List<string> Options_ContactedBy = new List<string>();
+        public List<string> Options_Impact = new List<string>();
+        public List<string> Options_StatusUpdate = new List<string>();
+        public List<string> Options_RequireMitigatingAction = new List<string>();
+        public List<string> Options_MitigatingAction = new List<string>();
+
+        #endregion
+
+        #region Public Method
+
+        public bool IsReadyForClose()
+        {
+            // check checklist's compulsory actions had been completed or not
+            foreach (ChecklistModel checklist in Checklists) if (!checklist.IsAllCompulsoryCompleted()) return false;
+
+            return true;
+        }
+        public IncidentStatus GetIncidentStatus()
+        {
+            // original status
+            IncidentStatus result = Entity.IncidentStatus.ToEnum<IncidentStatus>();
+
+            // Ready For Close
+            if (IsReadyForClose()) result = IncidentStatus.Ready_For_Close;
+
+            return result;
+        }
+        public void PrepareSave(string saveMode = "Last updated")
+        {
+            DateTime now = DateTime.Now;
+            Entity.History = AuditTrailService.GetHistory(Entity.History, AccessControlService.CurrentUser.GetLastUpdatedBy(), now, saveMode);
+
+            switch (saveMode.ToUpper())
+            {
+                case "CREATED":
+                    Entity.CreatedBy = AccessControlService.CurrentUser.GetLastUpdatedBy();
+                    Entity.CreatedDateTime = now;
+                    Entity.LastUpdatedBy = AccessControlService.CurrentUser.GetLastUpdatedBy();
+                    Entity.LastUpdatedDateTime = now;
+                    Entity.IssueById = AccessControlService.CurrentUser.Entity.UserId;
+                    Entity.IssueDateTime = now;
+
+                    Entity.IncidentNo = IncidentService.GetNewIncidentNo(SequenceType.Incident, DateTime.Now.Year);
+
+                    Entity.LevelOfSeverity = IncidentService.GetIncidentLevel(Entity) == IncidentLevel.None ? Entity.LevelOfSeverity : (Convert.ToInt32(IncidentService.GetIncidentLevel(Entity))).ToString();
+                    break;
+                case "LAST UPDATED":
+                    Entity.LastUpdatedBy = AccessControlService.CurrentUser.GetLastUpdatedBy();
+                    Entity.LastUpdatedDateTime = now;
+
+                    Entity.IncidentStatus = GetIncidentStatus().ToEnumString();
+
+                    Entity.LevelOfSeverity = IncidentService.GetIncidentLevel(Entity) == IncidentLevel.None ? Entity.LevelOfSeverity : (Convert.ToInt32(IncidentService.GetIncidentLevel(Entity))).ToString();
+                    break;
+                case "CLOSED":
+                    Entity.LastUpdatedBy = AccessControlService.CurrentUser.GetLastUpdatedBy();
+                    Entity.LastUpdatedDateTime = now;
+                    Entity.CloseById = AccessControlService.CurrentUser.Entity.UserId;
+                    Entity.CloseDateTime = now;
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Private Method
+
+        private void initOptions()
+        {
+            this.Options_ProblemArea = InputOptionsService.GetIncidentSystemInvoicingInputOptions(IncidentSystemInvoicingInputKey.IncidentSystemInvoicing_ProblemArea);
+            this.Options_PossibleCause = InputOptionsService.GetIncidentSystemInvoicingInputOptions(IncidentSystemInvoicingInputKey.IncidentSystemInvoicing_PossibleCause);
+            this.Options_ContactedBy = InputOptionsService.GetIncidentSystemInvoicingInputOptions(IncidentSystemInvoicingInputKey.IncidentSystemInvoicing_ContactedBy);
+            this.Options_Impact = InputOptionsService.GetIncidentSystemInvoicingInputOptions(IncidentSystemInvoicingInputKey.IncidentSystemInvoicing_Impact);
+            this.Options_StatusUpdate = InputOptionsService.GetIncidentSystemInvoicingInputOptions(IncidentSystemInvoicingInputKey.IncidentSystemInvoicing_StatusUpdate);
+            this.Options_RequireMitigatingAction = InputOptionsService.GetIncidentSystemInvoicingInputOptions(IncidentSystemInvoicingInputKey.IncidentSystemInvoicing_RequireMitigatingAction);
+            this.Options_MitigatingAction = InputOptionsService.GetIncidentSystemInvoicingInputOptions(IncidentSystemInvoicingInputKey.IncidentSystemInvoicing_MitigatingAction);
+        }
+
+        #endregion
+    }
+}
---
+++ CCCP/Controllers/IncidentSystemInvoicingController.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CCCP.ViewModel;
+using CCCP.Business.Model;
+using CCCP.Business.Service;
+using CCCP.Common;
+
+namespace CCCP.Controllers
+{
+    public class IncidentSystemInvoicingController : Controller
+    {
+        private CCCPDbContext db = new CCCPDbContext();
+        public IncidentSystemInvoicingModel incident = new IncidentSystemInvoicingModel();
+
+        // GET: IncidentSystemInvoicings
+        public ActionResult Index()
+        {
+            return View(db.IncidentSystemInvoicing.ToList());
+        }
+
+        // GET: IncidentSystemInvoicings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IncidentSystemInvoicing incidentSystemInvoicing = db.IncidentSystemInvoicing.Find(id);
+            if (incidentSystemInvoicing == null)
+            {
+                return HttpNotFound();
+            }
+            LoadData(id.Value);
+            if (Session != null)
+            {
+                Session["incident"] = incident;
+            }
+
+            return View(incident);
+        }
+
+        // GET: IncidentSystemInvoicings/Create
+        public ActionResult Create()
+        {
+            return View(incident);
+        }
+
+        // POST: IncidentSystemInvoicings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IncidentSystemInvoicingId,ChecklistBatchId,ChatRoomId,GeneralEnquiryId,CrisisId,NotificationId,IssueById,IssueDateTime,CloseById,CloseDateTime,IncidentNo,LevelOfSeverity,IncidentStatus,IncidentBackground,IsDrillMode,ProblemArea,PossibleCause,ExpectedAffectedNoOfBill,ExpectedAffectedBillingDay,ContactedBy,Impact,StatusUpdate,RequireMitigatingAction,MitigatingAction,CreatedBy,CreatedDateTime,LastUpdatedBy,LastUpdatedDateTime")] IncidentSystemInvoicing incidentSystemInvoicing)
+        {
+            if (ModelState.IsValid)
+            {
+                incidentSystemInvoicing.IncidentStatus = Common.IncidentStatus.Pending.ToString();
+                incident.Entity = incidentSystemInvoicing;
+                incident.PrepareSave("Created");
+                incidentSystemInvoicing = incident.Entity;
+
+                db.IncidentSystemInvoicing.Add(incidentSystemInvoicing);
+                db.SaveChanges();
+                db.usp_Incident_PostCreate(incidentSystemInvoicing.IncidentSystemInvoicingId, 7, incident.Entity.CreatedBy, Common.CheckListActionStatus.Pending.ToString());
+                return RedirectToAction("Index");
+            }
+
+            return View(incidentSystemInvoicing);
+        }
+
+        // GET: IncidentSystemInvoicings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            IncidentSystemInvoicing incidentSystemInvoicing = db.IncidentSystemInvoicing.Find(id);
+            if (incidentSystemInvoicing == null) return HttpNotFound();
+            else
+            {
+                LoadData(id.Value);
+                if (Session != null)
+                {
+                    Session["incident"] = incident;
+                }
+
+                return View(incident);
+            }
+        }
+
+        // POST: IncidentSystemInvoicings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IncidentSystemInvoicingId,ChecklistBatchId,ChatRoomId,GeneralEnquiryId,CrisisId,NotificationId,IssueById,IssueDateTime,CloseById,CloseDateTime,IncidentNo,LevelOfSeverity,IncidentStatus,IncidentBackground,IsDrillMode,History,ProblemArea,PossibleCause,ExpectedAffectedNoOfBill,ExpectedAffectedBillingDay,ContactedBy,Impact,StatusUpdate,RequireMitigatingAction,MitigatingAction,CreatedBy,CreatedDateTime,LastUpdatedBy,LastUpdatedDateTime")] IncidentSystemInvoicing incidentSystemInvoicing)
+        {
+                if (Session != null && Session["incident"] != null)
+                {
+                    incident = Session["incident"] as IncidentSystemInvoicingModel;
+                    incident.Entity = incidentSystemInvoicing;
+                }
+                
+            if (ModelState.IsValid)
+            {
+                // prepare history etc. before save
+                if (incident.Entity.IncidentStatus == IncidentStatus.Closed.ToEnumString())
+                {
+                    incident.PrepareSave("Closed");
+                }
+                else
+                {
+                    incident.PrepareSave();
+                }
+
+                if (Session != null && Session["incident"] != null)
+                {
+                    db.IncidentSystemInvoicing.Attach(incident.Entity);
+                    foreach (ChecklistModel cl in incident.Checklists)
+                    {
+                        foreach (ChecklistActionModel clAction in cl.ChecklistActions)
+                        {
+                            db.ChecklistAction.Attach(clAction.Entity);
+                            db.Entry(clAction.Entity).State = EntityState.Modified;
+                        }
+                    }
+                }
+                db.Entry(incident.Entity).State = EntityState.Modified;
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (System.Data.Entity.Validation.DbEntityValidationException ex)
+                {
+                    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                    foreach (var failure in ex.EntityValidationErrors)
+                    {
+                        sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+                        foreach (var error in failure.ValidationErrors)
+                        {
+                            sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+                            sb.AppendLine();
+                        }
+                    }
+
+                    // output validation errors
+                    Console.WriteLine(sb.ToString());
+                }
+
+                return RedirectToAction("Details", new { id = incident.Entity.IncidentSystemInvoicingId });
+            }
+            return View(incident);
+        }
+
+        // GET: IncidentSystemInvoicings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IncidentSystemInvoicing incidentSystemInvoicing = db.IncidentSystemInvoicing.Find(id);
+            if (incidentSystemInvoicing == null)
+            {
+                return HttpNotFound();
+            }
+            LoadData(id.Value);
+            return View(incident);
+        }
+
+        // POST: IncidentSystemInvoicings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            IncidentSystemInvoicing incidentSystemInvoicing = db.IncidentSystemInvoicing.Find(id);
+            db.IncidentSystemInvoicing.Remove(incidentSystemInvoicing);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public void LoadData(int incidentId)
+        {
+            incident = new IncidentSystemInvoicingModel();
+
+            // load incident details
+            incident.Entity = db.IncidentSystemInvoicing.Find(incidentId);
+
+            // load checklists
+            int checklistBatchID = incident.Entity.ChecklistBatchId;
+            incident.ChecklistEntities = (from c in db.Checklist                                          
+                                          where c.ChecklistBatchId.Equals(checklistBatchID)
+                                          orderby c.SortingOrder
+                                          select c).ToList<Checklist>();
+
+            // load checklist actions
+            foreach (ChecklistModel checklist in incident.Checklists)
+            {
+                List<ChecklistAction> actionEntities = (from ca in db.ChecklistAction
+                                                        where ca.ChecklistId.Equals(checklist.Entity.ChecklistId)
+                                                        orderby ca.SortingOrder
+                                                        select ca).ToList<ChecklistAction>();
+                checklist.ChecklistActionEntities = actionEntities;
+            }
+
+            // load chat room
+
+        }
+
+        public ActionResult ToggleActionStatus(int checklist, int checklistAction)
+        {
+            if (Session != null && Session["incident"] != null)
+            {
+                incident = Session["incident"] as IncidentSystemInvoicingModel;
+            }
+
+            incident.Checklists[checklist].ChecklistActions[checklistAction].ToggleActionStatus();
+
+            if (Session != null)
+            {
+                Session["incident"] = incident;
+            }
+
+            return Json(new { result = true });
+        }
+
+        public ActionResult CloseIncident()
+        {
+            if (Session != null && Session["incident"] != null)
+            {
+                incident = Session["incident"] as IncidentSystemInvoicingModel;
+            }
+            incident.Entity.IncidentStatus = IncidentStatus.Closed.ToEnumString();
+
+            return Edit(incident.Entity);
+        }
+
+        public void Test()
+        {
+            incident.Checklists[1].ChecklistActions[1].ToggleActionStatus();
+        }
+    }
+}
