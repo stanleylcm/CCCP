4f00d9d4d0852dde344f38e703bcdd9888eba397
kf-wang <kfwang4@gmail.com>
2016-03-04 16:50:51 +0800

    draft

--- CCCP.ViewModel/Metadata.cs
+++ CCCP.ViewModel/Metadata.cs
@@ -145,4 +145,67 @@ namespace CCCP.ViewModel
         public Nullable<System.DateTime> LastUpdatedDateTime { get; set; }
     }
     #endregion
+
+    #region Incident System Call Centre Metadata
+    [MetadataType(typeof(IncidentSystemCallCentreHelper))]
+    public partial class IncidentSystemCallCentre { }
+
+    public partial class IncidentSystemCallCentreHelper
+    {
+        [Display(Name = "ID")]
+        public int IncidentSystemCallCentreId { get; set; }
+        [Display(Name = "Checklist Batch ID")]
+        public int ChecklistBatchId { get; set; }
+        [Display(Name = "ChatRoom ID")]
+        public int ChatRoomId { get; set; }
+        [Display(Name = "General Enquiry ID")]
+        public int GeneralEnquiryId { get; set; }
+        [Display(Name = "Crisis ID")]
+        public Nullable<int> CrisisId { get; set; }
+        [Display(Name = "Notification ID")]
+        public int NotificationId { get; set; }
+        [Display(Name = "Issue By")]
+        public Nullable<int> IssueById { get; set; }
+        [Display(Name = "Issue Date Time")]
+        public Nullable<System.DateTime> IssueDateTime { get; set; }
+        [Display(Name = "Close By")]
+        public Nullable<int> CloseById { get; set; }
+        [Display(Name = "Close Date Time")]
+        public Nullable<System.DateTime> CloseDateTime { get; set; }
+        [Display(Name = "Incident No")]
+        public string IncidentNo { get; set; }
+        [Display(Name = "Level Of Severity")]
+        public string LevelOfSeverity { get; set; }
+        [Display(Name = "Incident Status")]
+        public string IncidentStatus { get; set; }
+        [Display(Name = "Incident Background")]
+        public string IncidentBackground { get; set; }
+        [Display(Name = "Is Drill Mode?")]
+        public Nullable<bool> IsDrillMode { get; set; }
+        [Display(Name = "History")]
+        public string History { get; set; }
+        [Display(Name = "Impact")]
+        public string Impact { get; set; }
+        [Display(Name = "Possible Cause")]
+        public string PossibleCause { get; set; }
+        [Display(Name = "Expected Restoration Time")]
+        public Nullable<short> ExpectedRestorationTime { get; set; }
+        [Display(Name = "Require Mitigating Action")]
+        public string RequireMitigatingAction { get; set; }
+        [Display(Name = "Mitigating Action")]
+        public string MitigatingAction { get; set; }
+        [Display(Name = "Status Update")]
+        public string StatusUpdate { get; set; }
+        [Display(Name = "Full Restoration")]
+        public Nullable<System.DateTime> FullRestoration { get; set; }
+        [Display(Name = "Created By")]
+        public string CreatedBy { get; set; }
+        [Display(Name = "Created Date Time")]
+        public Nullable<System.DateTime> CreatedDateTime { get; set; }
+        [Display(Name = "Last Updated By")]
+        public string LastUpdatedBy { get; set; }
+        [Display(Name = "Last Updated Date Time")]
+        public Nullable<System.DateTime> LastUpdatedDateTime { get; set; }
+    }
+    #endregion
 }
