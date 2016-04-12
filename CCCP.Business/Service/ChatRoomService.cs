769a490d73a7c20fdf853ce887d99d24fd15a50c
kf-wang <kfwang4@gmail.com>
2016-04-12 10:58:27 +0800

    draft

---
+++ CCCP.Business/Service/ChatRoomService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CCCP.Common;
+using CCCP.Business.Model;
+using CCCP.ViewModel;
+
+namespace CCCP.Business.Service
+{
+    public class ChatRoomService
+    {   
+        public static void SaveChatMessage(int chatRoomId, int userId, String message, DateTime sendDateTime)
+        {
+            CCCPDbContext db = new CCCPDbContext();
+            UserModel user = new UserModel(db.User.Find(userId));
+            ChatRoomMessageModel msgModel = new ChatRoomMessageModel();
+
+            msgModel.Entity.ChatRoomId = chatRoomId;
+            msgModel.Entity.SenderUserId = userId;
+            msgModel.Entity.SenderDisplayName = user.Entity.DisplayName;
+            msgModel.Entity.SendDateTime = sendDateTime;
+            msgModel.Entity.Message = message;
+            msgModel.Entity.CreatedBy = user.GetLastUpdatedBy();
+            msgModel.Entity.CreatedDateTime = DateTime.Now;
+            msgModel.Entity.LastUpdatedBy = user.GetLastUpdatedBy();
+            msgModel.Entity.LastUpdatedDateTime = DateTime.Now;
+
+            db.ChatRoomMessage.Add(msgModel.Entity);
+            db.SaveChanges();
+        }
+    }
+}
--- CCCP/Hubs/ChatRoomHub.cs
+++ CCCP/Hubs/ChatRoomHub.cs
@@ -3,29 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using CCCP.Business.Service;
 
 namespace CCCP.Hubs
 {
     public class ChatRoomHub : Hub
     {
-        public void Hello()
+        public void Send(string name, string message, string time, int userId, int chatRoomId)
         {
-            Clients.All.hello();
+            ChatRoomService.SaveChatMessage(chatRoomId, userId, message, Convert.ToDateTime(time));
+            Clients.All.broadcastMessage(name, message, time, chatRoomId);
         }
 
-        public void Send(string name, string message, string time)
+        public void SendImage(string name, string imageUrl, string imageResizeUrl, string time, int userId, int chatRoomId)
         {
-            Clients.All.broadcastMessage(name, message, time);
+            Clients.All.broadcastImageMessage(name, imageUrl, imageResizeUrl, time, chatRoomId);
         }
 
-        public void SendImage(string name, string imageUrl, string imageResizeUrl, string time)
+        public void SendVideo(string name, string videoUrl, string time, int userId, int chatRoomId)
         {
-            Clients.All.broadcastImageMessage(name, imageUrl, imageResizeUrl, time);
-        }
-
-        public void SendVideo(string name, string videoUrl, string time)
-        {
-            Clients.All.broadcastVideoMessage(name, videoUrl, time);
+            Clients.All.broadcastVideoMessage(name, videoUrl, time, chatRoomId);
         }
     }
 }
