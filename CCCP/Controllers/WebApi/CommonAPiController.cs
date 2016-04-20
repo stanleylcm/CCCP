140f195749a923456899ac660dab2e06da9192ef
kf-wang <kfwang4@gmail.com>
2016-04-20 18:06:06 +0800

    update webapi for chatroom

--- CCCP/Controllers/WebApi/CommonAPiController.cs
+++ CCCP/Controllers/WebApi/CommonAPiController.cs
@@ -22,14 +22,38 @@ namespace CCCP.Controllers.WebApi
 
         [System.Web.Http.HttpGet]
         [System.Web.Http.HttpPost]
-        public Boolean SendChatRoomMessage(string name, string message, string time, int userId, int chatRoomId, IEnumerable<HttpPostedFileBase> uploadData)
+        public int SendChatRoomMessage(string name, string message, string time, int userId, int chatRoomId, IEnumerable<HttpPostedFileBase> uploadData)
         {
             JsonResult result = new CCCP.Controllers.ChatRoomController().SaveChatRoomMessageAttachment(chatRoomId, userId, name, Convert.ToDateTime(time), message, uploadData);
 
             dynamic response = Newtonsoft.Json.JsonConvert.DeserializeObject(result.ToString());
             new CCCP.Hubs.ChatRoomHub().Send(name, message, time, userId, chatRoomId, response.id);
             
-            return true;
+            return response.id;
+        }
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.HttpPost]
+        public List<ChatRoomMessageModel> GetLatestMessages(int chatRoomId, int lastMessageId)
+        {
+            CCCPDbContext db = new CCCPDbContext();
+
+            List<ChatRoomMessageModel> messageList = (from crMessage in db.ChatRoomMessage
+                                                      where crMessage.ChatRoomId.Equals(chatRoomId) && crMessage.ChatRoomMessageId > lastMessageId
+                                                      orderby crMessage.SendDateTime
+                                                      select new ChatRoomMessageModel()
+                                                      {
+                                                          Entity = crMessage
+                                                      }).ToList<ChatRoomMessageModel>();
+
+            foreach (ChatRoomMessageModel crMessage in messageList)
+            {
+                crMessage.ChatRoomAttachmentsEntities = (from crAttachment in db.ChatRoomAttachment
+                                                         where crAttachment.ChatRoomMessageId.Equals(crMessage.Entity.ChatRoomMessageId)
+                                                         select crAttachment).ToList<ChatRoomAttachment>();
+            }
+
+            return messageList;
         }
     }
 }
